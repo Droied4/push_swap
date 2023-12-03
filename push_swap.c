5d7a7198ccd85ef61b49a56de3b8369548099e45
Carmen Ordoñez <deiord44santiagoov@gmail.com>
2023-12-04 00:52:56 +0100

    todo ok

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: deordone <deordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:48:57 by deordone          #+#    #+#             */
-/*   Updated: 2023/11/29 15:42:38 by deordone         ###   ########.fr       */
+/*   Updated: 2023/12/04 00:52:37 by carmeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ int	push_swap(int argc, char **argv, t_list **lst)
 		j++;
 		i++;
 	}
-	if (ft_iscorrect(lst) < 1)
+	if (ft_isrepeat(lst) < 1)
 		return (-1);
+	printf("todo ok");
+
 	return (1);
 }
 
